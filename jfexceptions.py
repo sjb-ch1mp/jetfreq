70ab1c96ddff78db262ba091e125f5da8c4d35e5
ch1mp <53543229+sjb-ch1mp@users.noreply.github.com>
2020-07-09 17:20:27 +1000

    Update jfexceptions.py

--- jfexceptions.py
+++ jfexceptions.py
@@ -1,52 +1,52 @@
 class Error(Exception):
-        pass
+	pass
 
 class IncorrectUsageError(Error):
-        def __init__(self, context):
-                self.context = context
+	def __init__(self, context):
+		self.context = context
 
 class IncorrectModuleError(Error):
-        def __init__(self, module):
-                self.module = module
+	def __init__(self, module):
+		self.module = module
 
 class NoArgsError(Error):
-        def __init__(self, message):
-                self.message = message
+	def __init__(self, message):
+		self.message = message
 
 class NoSuchArgumentError(Error):
-        def __init__(self, flag):
-                self.flag = flag
+	def __init__(self, flag):
+		self.flag = flag
 
 class MissingValueError(Error):
-        def __init__(self, flag, name):
-                self.flag = flag
-                self.name = name
+	def __init__(self, flag, name):
+		self.flag = flag
+		self.name = name
 
 class DoubleArgumentError(Error):
-        def __init__(self, flag):
-                self.flag = flag
+	def __init__(self, flag):
+		self.flag = flag
 
 class FlagsNotApplicableError(Error):
-        def __init__(self, mode, flags):
-                self.mode = mode
-                self.flags = flags
+	def __init__(self, mode, flags):
+		self.mode = mode
+		self.flags = flags
 
 class APIQueryError(Error):
-        def __init__(self, message):
-                self.message = message
+	def __init__(self, message):
+		self.message = message
 
 class MissingConfigurationError(Error):
-        def __init__(self):
-                self.message = "You need to configure jetfreq by adding the Carbon Black Server name and API Key to ./conf/jetfreq.cfg"
+	def __init__(self):
+		self.message = "You need to configure jetfreq by adding the Carbon Black Server name and API Key to ./conf/jetfreq.cfg"
 
 class UnexpectedResponseError(Error):
-        def __init__(self, data):
-                self.data = data
+	def __init__(self, data):
+		self.data = data
 
 class NoResultsError(Error):
-        def __init__(self, query):
-                self.query = query
+	def __init__(self, query):
+		self.query = query
 
 class NoEventsFoundError(Error):
-        def __init__(self, query):
-                self.query = query
+	def __init__(self, query):
+		self.query = query
