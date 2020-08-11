691d2910df383942a9e1a7299d9eba0c490ea064
ch1mp <53543229+sjb-ch1mp@users.noreply.github.com>
2020-08-12 09:43:25 +1000

    jfanalyze.py Version 1

--- jfanalyze.py
+++ jfanalyze.py
@@ -1,21 +1,27 @@
+# MODULE CONTAINS ALL FUNCTIONS THAT CONDUCT FREQUENCY ANALYSIS ON 
+# THE RESULTS OF A CARBON BLACK QUERY
+
 import jfutil
 import jfexceptions
 import re
 import json
 
+# CONTAINER CLASS FOR EVENT FREQUENCIES
 class EventFreq():
 	def __init__(self, path, perc, count, total):
-		self.path = path
-		self.perc = perc
-		self.count = count
-		self.total = total
+		self.path = path # PATH OF EVENT
+		self.perc = perc # PERCENTAGE OF PROCESSES IN WHICH EVENT OCCURS
+		self.count = count # THE NUMBER OF PROCESSES IN WHICH EVENT OCCURS
+		self.total = total # THE TOTAL NUMBER OF PROCESSES THAT CONTAIN AN EVENT OF THIS TYPE
 
+# CONTAINER CLASS FOR EVENT DIFFERENCES
 class EventDiff():
 	def __init__(self, target_event, representative_event, difftype):
-		self.target_event = target_event
-		self.representative_event = representative_event
-		self.difftype = difftype
-	
+		self.target_event = target_event # AN EVENT FROM THE TARGET SAMPLE
+		self.representative_event = representative_event # THE CORRESPONDING EVENT FROM THE REPRESENTATIVE SAMPLE
+		self.difftype = difftype # THE TYPE OF DIFFERENCE BETWEEN THE EVENTS
+
+# CONTAINER CLASS FOR DIFFERENCE TYPES
 class DiffType(): 
 	def __init__(self):
 		self.MISS_FM_REP = 'MISSING FROM REPRESENTATIVE SAMPLE'
@@ -35,6 +41,7 @@ class DiffType():
 		self.TAR_MISS_D = 'TARGET SAMPLE HAS NO NETCONNS, BUT REPRESENTATIVE SAMPLE DOES'
 		self.TAR_MISS_X = 'TARGET SAMPLE HAS NO CROSSPROCS, BUT REPRESENTATIVE SAMPLE DOES'
 	
+	# TAKES AN EVENT TYPE AND RETURNS THE CORRESPONDING DIFFERENCE TYPE
 	def get_diff_type_by_event(event_type, sample):
 		if sample == 'rep':
 			if event_type == 'modloads':
@@ -63,7 +70,7 @@ class DiffType():
 			else:	
 				return self.TAR_MISS_X
 		
-
+# ORDER A LIST OF EVENTS ALPHABETICALLY
 def alphabetize_events(events):
 	for i in range(len(events)):
 		j = i
@@ -74,7 +81,8 @@ def alphabetize_events(events):
 				events[j] = hold
 			j = j + 1
 	return events
-	
+
+# ORDER A LIST OF EVENTS BY THEIR FREQUENCY
 def sort_events(events):
 	for i in range(len(events)):
 		j = i
@@ -86,15 +94,27 @@ def sort_events(events):
 			j = j + 1
 	return events
 
-def calculate_event_frequency(event_counts, total_processes, threshold):
+# CALCULATE THE FREQUENCY OF A GIVEN EVENT
+def calculate_event_frequency(event_counts, total_processes, greater_than, less_than):
 	events = []
 	for key in event_counts:
 		perc = round((event_counts[key]/float(total_processes)), 5)
-		if perc * 100 <= float(threshold):
-			events.append(EventFreq(key, perc, event_counts[key], total_processes))
+		if greater_than == None:
+			if perc * 100 <= float(less_than):
+				events.append(EventFreq(key, perc, event_counts[key], total_processes))
+		elif less_than == None:
+			if perc * 100 >= float(greater_than):
+				events.append(EventFreq(key, perc, event_counts[key], total_processes))
+		elif float(greater_than) > float(less_than): # both are not None
+			if perc * 100 <= float(less_than) or perc * 100 >= float(greater_than):
+				events.append(EventFreq(key, perc, event_counts[key], total_processes))
+		else:
+			if perc * 100 <= float(less_than) and perc * 100 >= float(greater_than):
+				events.append(EventFreq(key, perc, event_counts[key], total_processes))
 	events = sort_events(events)
 	return events
 
+# COUNT HOW MANY TIMES AN EVENT OCCURS IN A GIVEN PROCESS
 def count_events(process, container, key):
 	for event in process[key]:
 		if event in container:
@@ -103,10 +123,14 @@ def count_events(process, container, key):
 			container[event] = 1
 	return container
 
+# THE MAIN FUNCTION FOR THE --BY-PROCESS AND --COMPARE-PROCESS MODES
+# THIS FUNCTION TAKES THE DATA RETURNED FROM CARBON BLACK AND CALCULATES
+# THE FREQUENCY IN WHICH THE SPECIFIED EVENT TYPES OCCUR ACROSS PROCESSES
+# IN THE SAMPLE
 def analyze_by_process(params, data):
 	jfutil.debug(params['verbose'], "Conducting analysis for {}".format(params['search_name']))
 
-	# do count
+	# INITIALIZE THE JSON CONTAINER FOR THE RESULTS OF THE EVENT COUNT
 	event_counts = {
 		'modloads':{},
 		'regmods':{},
@@ -115,6 +139,9 @@ def analyze_by_process(params, data):
 		'netconns':{},
 		'crossprocs':{}
 	}
+
+	# COUNT HOW MANY TIMES EACH SPECIFIC EVENT OCCURS ACROSS ALL 
+	# PROCESSES IN THE SAMPLE RETURNED FROM CARBON BLACK
 	for process in data:
 		if 'modloads' in process:
 			jfutil.debug(params['verbose'], 'Counting modloads')
@@ -136,7 +163,7 @@ def analyze_by_process(params, data):
 			event_counts['crossprocs'] = count_events(process, event_counts['crossprocs'], 'crossprocs')
 	jfutil.debug(params['verbose'], 'Count complete')
 
-	# calculate frequencies	and exclude processes that exceed threshold
+	# INITIALIZE THE JSON CONTAINER FOR THE RESULTS OF THE EVENT FREQUENCY ANALYSIS
 	event_freqs = {
 		'modloads':None,
 		'regmods':None,
@@ -145,32 +172,38 @@ def analyze_by_process(params, data):
 		'netconns':None,
 		'crossprocs':None
 	}
+
+	# USING THE COUNTS, CALCULATE THE FREQUENCY IN WHICH EACH EVENT OCCURS ACROSS
+	# PROCESSES IN THE SAMPLE RETURNED BY CARBON BLACK
 	if len(event_counts['modloads']) > 0:
 		jfutil.debug(params['verbose'], 'Calculating freq of modloads')
-		event_freqs['modloads'] = calculate_event_frequency(event_counts['modloads'], len(data), params['threshold'])
+		event_freqs['modloads'] = calculate_event_frequency(event_counts['modloads'], len(data), params['threshold_gt'], params['threshold_lt'])
 	if len(event_counts['regmods']) > 0:
 		jfutil.debug(params['verbose'], 'Calculating freq of regmods')
-		event_freqs['regmods'] = calculate_event_frequency(event_counts['regmods'], len(data), params['threshold'])
+		event_freqs['regmods'] = calculate_event_frequency(event_counts['regmods'], len(data), params['threshold_gt'], params['threshold_lt'])
 	if len(event_counts['childprocs']) > 0:
 		jfutil.debug(params['verbose'], 'Calculating freq of childprocs')
-		event_freqs['childprocs'] = calculate_event_frequency(event_counts['childprocs'], len(data), params['threshold'])
+		event_freqs['childprocs'] = calculate_event_frequency(event_counts['childprocs'], len(data), params['threshold_gt'], params['threshold_lt'])
 	if len(event_counts['filemods']) > 0:
 		jfutil.debug(params['verbose'], 'Calculating freq of filemods')
-		event_freqs['filemods'] = calculate_event_frequency(event_counts['filemods'], len(data), params['threshold'])
+		event_freqs['filemods'] = calculate_event_frequency(event_counts['filemods'], len(data), params['threshold_gt'], params['threshold_lt'])
 	if len(event_counts['netconns']) > 0:
 		jfutil.debug(params['verbose'], 'Calculating freq of netconns')
-		event_freqs['netconns'] = calculate_event_frequency(event_counts['netconns'], len(data), params['threshold'])
+		event_freqs['netconns'] = calculate_event_frequency(event_counts['netconns'], len(data), params['threshold_gt'], params['threshold_lt'])
 	if len(event_counts['crossprocs']) > 0:
 		jfutil.debug(params['verbose'], 'Calculating freq of crossprocs')
-		event_freqs['crossprocs'] = calculate_event_frequency(event_counts['crossprocs'], len(data), params['threshold'])
+		event_freqs['crossprocs'] = calculate_event_frequency(event_counts['crossprocs'], len(data), params['threshold_gt'], params['threshold_lt'])
 	jfutil.debug(params['verbose'], "Freq calculations complete")
 	
 	return event_freqs
 
+# THIS IS THE MAIN FUNCTION FOR --COMPARE-EVENT AND --BY-EVENT MODES
+# THIS FUNCTION CALCULATES THE FREQUENCY IN WHICH EACH PROCESS IN A SAMPLE
+# CREATES A GIVEN EVENT
 def analyze_by_event(params, data):
-	# event = {process:"", id:"", segment_id:""}
 	jfutil.debug(params['verbose'], "Conducting analysis for {}".format(params['search_name']))
-
+	
+	# COUNT HOW MANY PROCESSES CREATE A GIVEN EVENT IN THE SAMPLE
 	event_counts = {}
 	for process in data:
 		if not process['path'] in event_counts:
@@ -178,71 +211,96 @@ def analyze_by_event(params, data):
 		else:
 			event_counts[process['path']] = event_counts[process['path']] + 1
 	jfutil.debug(params['verbose'], "Count complete")
-
-	event_freqs = calculate_event_frequency(event_counts, len(data), params['threshold'])
+	
+	# CALCULATE THE FREQUENCY IN WHICH PROCESSES CREATE THE EVENT
+	event_freqs = calculate_event_frequency(event_counts, len(data), params['threshold_gt'], params['threshold_lt'])
 	jfutil.debug(params['verbose'], "Freq calculations complete")
 	
 	return event_freqs
 
+# THIS IS A UTILITY FUNCTION THAT LOADS A LIST INTO A 
+# DICTIONARY IN ORDER TO MAKE THE COMPARISON PROCESS 
+# MORE EFFICIENT
 def load_into_dict(events):
 	event_dict = {}
 	for event in events:
 		event_dict[event.path] = {'freq':event.perc,'object':event}
 	return event_dict
 
+# THIS IS THE MAIN FUNCTION FOR THE --COMPARE-PROCESS MODE
+# THIS FUNCTION COMPARES TWO SAMPLES, AND COLLATES THE DIFFERENCES
+# BETWEEN THE SAMPLES INTO DEFINED DIFFERENCE CATEGORIES
 def compare_process(params, representative_sample, target_sample):
-	
 	jfutil.debug(params['verbose'], 'Comparing target process {} to representative sample {}'.format(params['search_name'], params['import_sample']))	
+	
+	# INITIALIZE THE JSON CONTAINER FOR EVENT DIFFERENCES
 	event_diffs = {'modloads':[],'regmods':[],'childprocs':[],'filemods':[],'netconns':[],'crossprocs':[]}
 	difftype = DiffType()
 	
+	# FOR EACH EVENT TYPE
 	for key in event_diffs:
+		# IF THE EVENT TYPE EXISTS IN THE REP SAMPLE AND DOES NOT EXIST IN THE TAR SAMPLE
 		if not representative_sample[key] == None and target_sample[key] == None:
+			# FOR ALL EVENTS OF THIS TYPE IN THE REP SAMPLE, ADD AN APPROPRIATE 'MISSING FROM TAR' DIFF TYPE
 			for event in representative_sample[key]:
 				event_diffs[key].append(EventDiff(None, event, difftype.get_diff_type_by_event(key, 'rep')))
+		# IF THE EVENT TYPE EXISTS IN THE TAR SAMPLE AND DOES NOT EXIST IN THE REP SAMPLE
 		elif not target_sample[key] == None and representative_sample[key] == None:
+			# FOR ALL EVENTS OF THIS TYPE IN THE TAR SAMPLE, ADD AN APPROPRIATE 'MISSING FROM REP' DIFF TYPE
 			for event in target_sample[key]:
 				event_diffs[key].append(EventDiff(None, event, difftype.get_diff_type_by_event(key, 'tar')))
+		# IF THE EVENT TYPE EXISTS IN BOTH THE REP AND TAR SAMPLES
 		elif not target_sample[key] == None and not representative_sample[key] == None:
+			# LOAD THE EVENT LIST INTO A DICTIONARY
 			rep_dict = load_into_dict(representative_sample[key])
 			tar_dict = load_into_dict(target_sample[key])
-			# check for target samples missing from rep sample
+			# FOR EACH EVENT OF THIS TYPE IN THE TAR SAMPLE
 			for sub_key in tar_dict:
+				# IF THE EVENT DOES NOT EXIST IN THE REP SAMPLE, ADD A 'MISSING FROM REP' DIFF TYPE
 				if not sub_key in rep_dict:
 					event_diffs[key].append(EventDiff(tar_dict[sub_key]['object'], None, difftype.MISS_FM_REP))
 				else:
+					# IF THE EVENT EXISTS IN BOTH SAMPLES, CALCULATE WHETHER IT OCCURS MORE IN THE REP OR TAR SAMPLES
 					if float(tar_dict[sub_key]['freq']) > float(rep_dict[sub_key]['freq']):
 						event_diffs[key].append(EventDiff(tar_dict[sub_key]['object'], rep_dict[sub_key]['object'], difftype.HIGH_FQ_TAR))
 					elif float(tar_dict[sub_key]['freq']) < float(rep_dict[sub_key]['freq']):
 						event_diffs[key].append(EventDiff(tar_dict[sub_key]['object'], rep_dict[sub_key]['object'], difftype.HIGH_FQ_REP))
-			# check for rep samples missing from tar sample
+			# FOR EACH EVENT OF THIS TYPE IN THE REP SAMPLE
 			for sub_key in rep_dict:
+				# IF THE EVENT DOES NOT EXIST IN THE TAR SAMPLE, ADD A 'MISSING FROM TAR' DIFF TYPE
 				if not sub_key in tar_dict:
 					event_diffs[key].append(EventDiff(None, rep_dict[sub_key]['object'], difftype.MISS_FM_TAR))
 	jfutil.debug(params['verbose'], 'Comparison complete')
 	return event_diffs	
 
-
+# THIS IS THE MAIN FUNCTION FOR THE --COMPARE-EVENT MODE
+# THIS FUNCTION COMPARES TWO SAMPLES, AND COLLATES THE DIFFERENCES
+# BETWEEN THE SAMPLES INTO DEFINED DIFFERENCE CATEGORIES
 def compare_event(params, representative_sample, target_sample):
 
 	jfutil.debug(params['verbose'], 'Comparing target event {} to representative sample {}'.format(params['search_name'], params['import_sample']))
+	
+	# INITIALIZE THE JSON CONTAINER FOR PROCESS DIFFERENCES
 	event_diffs = []
 	difftype = DiffType()
 	rep_dict = load_into_dict(representative_sample)
 	tar_dict = load_into_dict(target_sample)
 	
-	# check for target samples missing from representative sample (new processes spawning an event)
+	# FOR EACH PROCESS IN THE TAR SAMPLE
 	for key in tar_dict:
+		# IF THE PROCESS DOES NOT EXIST IN THE REP SAMPLE, ADD A 'MISSING FROM REP' DIFF TYPE
 		if not key in rep_dict:
 			event_diffs.append(EventDiff(tar_dict[key]['object'], None, difftype.MISS_FM_REP))
 		else:
-			if float(tar_dict[key]['freq']) > float(rep_dict[key]['freq']): # check if there is a higher frequency in the target sample
+			# IF THE EVENT EXISTS IN BOTH SAMPLES, CALCULATE WHETHER IT OCCURS MORE IN THE REP OR TAR SAMPLES
+			if float(tar_dict[key]['freq']) > float(rep_dict[key]['freq']): 
 				event_diffs.append(EventDiff(tar_dict[key]['object'], rep_dict[key]['object'], difftype.HIGH_FQ_TAR))
-			elif float(tar_dict[key]['freq']) < float(rep_dict[key]['freq']): # check if there is a higher frequency in the representative sample
+			elif float(tar_dict[key]['freq']) < float(rep_dict[key]['freq']):
 				event_diffs.append(EventDiff(tar_dict[key]['object'], rep_dict[key]['object'], difftype.HIGH_FQ_REP))
 	
-	# check for representative samples missing from target sample (process no longer spawning an event)
+	# FOR EACH PROCESS IN THE REP SAMPLE
 	for key in rep_dict:
+		# IF THE PROCESS DOES NOT EXIST IN THE TAR SAMPLE, ADD A 'MISSING FROM TAR' DIFF TYPE
 		if not key in tar_dict:
 			event_diffs.append(EventDiff(None, rep_dict[key]['object'], difftype.MISS_FM_TAR))
 	jfutil.debug(params['verbose'], 'Comparison complete')	
